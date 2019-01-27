9509893ea6d49c757253e6bbebd350e1f7ba5c5c
Feruza12 <f.latipova@student.inha.uz>
2019-01-28 00:57:14 +0500

    try

--- js/common.js
+++ js/common.js
@@ -1,2 +1,20 @@
 $(function() {
+	function ajaxTable(){
+  let table;
+  $.ajax({
+    url: 'json/file.json',
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function(data){
+      $(data).each(function(index, value){
+        table += '<tr><td>' + value.Ccy + '</td><td>' + value.Rate + '</td></tr>';
+      });
+      let th = $(".currency_table").find("table").html();
+      console.log(th);
+      $(".currency_table").find("table").html(th + table);
+    }
+  });
+}
+
 });
