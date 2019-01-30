c61bde9bfd0de3edb45ae773a4d3216cbebcf577
Feruza12 <f.latipova@student.inha.uz>
2019-01-30 10:58:15 +0500

    smth

--- js/common.js
+++ js/common.js
@@ -1,20 +1,3 @@
-$(function() {
-	function ajaxTable(){
-  let table;
-  $.ajax({
-    url: 'json/file.json',
-    dataType: 'json',
-    type: 'get',
-    cache: false,
-    success: function(data){
-      $(data).each(function(index, value){
-        table += '<tr><td>' + value.Ccy + '</td><td>' + value.Rate + '</td></tr>';
-      });
-      let th = $(".currency_table").find("table").html();
-      console.log(th);
-      $(".currency_table").find("table").html(th + table);
-    }
-  });
-}
-
+$(document).ready(function (){
+  
 });
