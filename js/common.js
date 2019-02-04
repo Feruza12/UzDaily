891b3798a68c6a7f1510f641723572b581dea209
Feruza12 <f.latipova@student.inha.uz>
2019-02-04 14:58:38 +0500

    will not change again

--- js/common.js
+++ js/common.js
@@ -1,3 +1,15 @@
 $(document).ready(function (){
+	var owl = $('.owl-carousel');
+	owl.owlCarousel({
+    items:1,
+    loop:true,
+    nav: true,
+    navText: ['<img src="./img/arrows/chevron-left.svg"/>','<img src="img/arrows/chevron-right.svg"/>'],
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+	});
+	
 });
 
